b22470251a02920f58507f52f76111a7783135ee
Maxime Leszczynski <maxime.leszczynski@laposte.net>
2019-10-30 11:45:00 +0100

    maj1

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,43 @@
 $(function() {
-  // Enjoy :)
+  $.ajax({
+    url : 'ajax/content.html', // La ressource ciblée
+    type : 'GET', // Le type de la requête HTTP
+    success:function(html){
+      console.log(html);
+      $('#img1').html(html);
+    },
+    error:function(response){
+      console.log('error')
+
+    }
+  });
+
+  $.ajax({
+    url : 'ajax/delay5.html', // La ressource ciblée
+    type : 'GET', // Le type de la requête HTTP
+    success:function(html){
+      console.log(html);
+      $('#img2').html(html);
+    },
+    error:function(response){
+      console.log('error')
+
+    }
+  });
+
+  $.ajax({
+    url : 'ajax/info.html', // La ressource ciblée
+    type : 'GET', // Le type de la requête HTTP
+    success:function(html){
+      console.log(html);
+      $('#bt3').click(function(){
+      $('#img3').html(html);
+    });
+    },
+    error:function(response){
+      console.log('error')
+
+    }
+  });
+
 });
